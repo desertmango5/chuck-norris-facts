81850c1e1e6a94c78fddd4fa896aea8450753e3c
Michael Manges <michael.manges@gmail.com>
2017-08-08 16:25:26 +0400

    Initial commit
    Created repository

---
+++ randomQuotes.js
@@ -0,0 +1,44 @@
+/*
+
+Random quote generator. Maybe a Chuck Norris Random Joke Generator??
+
+*/
+
+
+$(document).ready(function() {
+
+	var quote = {
+
+		setup: function() {
+			$.getJSON('http://api.icndb.com/jokes/random/', function(data) {
+					var quoteData = data.value.joke;
+					$('#quote').html(quoteData);
+					
+					var hashtags = "&hashtags=ChuckNorrisFacts,FCC";
+					var tweetURL = "https://twitter.com/intent/tweet?text=" + quoteData + hashtags;
+
+					$('#tweet').attr("href", tweetURL);
+	  	});
+		},
+		random: function() {
+			this.setup();
+		}		
+	};
+
+	$('#random').on('click', function() {
+			quote.random();
+		});
+
+	quote.random();	
+
+
+
+});
+
+
+// var tweetURL = "https://twitter.com/intent/tweet?text=" + quoteData + "?hashtags=chuchnorrisfacts"
+
+ // var tweetURL = `'https://twitter.com/intent/tweet?text=${quoteData}?hashtags=chuchnorrisfacts'`
+
+console.log()
+
